a8e94c5fe07286357afe577bf414df85bf50de86
m1llark <ponomarevm539@gmail.com>
2023-04-07 18:03:22 +0500

    kl

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -12,12 +12,10 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-
 
     @Column(name = "name")
     @NotEmpty(message = "Name should not be empty")
